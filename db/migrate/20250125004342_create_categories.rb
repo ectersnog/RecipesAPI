7da4f9763fbbd7aeff001d1acd570be0a9acdff7
Jason <jdamundsen@gmail.com>
2025-01-26 21:00:28 -0600

    schema.rb update

--- db/migrate/20250125004342_create_categories.rb
+++ db/migrate/20250125004342_create_categories.rb
@@ -5,11 +5,12 @@ class CreateCategories < ActiveRecord::Migration[8.0]
     create_table :categories, id: :uuid do |t|
       t.string :name, null: false
       t.string :slug, null: false
-      t.references :parent, null: true, foreign_key: { to_table: :categories }, type: :uuid
+      t.references :parent, null: true, foreign_key: { to_table: :categories, index: false }, type: :uuid
       t.timestamps
     end
 
     add_index :categories, :name
+    add_index :categories, :parent_id
     add_index :categories, :slug, unique: true
   end
 end
