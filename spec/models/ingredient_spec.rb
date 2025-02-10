e55682445e6b653cc7556baab693abe880bb5623
Jason <jdamundsen@gmail.com>
2025-02-10 16:37:46 -0600

    ingredient_spec fix

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Ingredient, type: do
   describe 'validations' do
-    it { is_expected.to allow_value("cheese").for(:name) }
+    it { is_expected.to allow_value("Cheese").for(:name) }
     it { is_expected.not_to allow_value(nil).for(:name) }
   end
 end
