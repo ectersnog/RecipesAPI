f10bd0190dc604019c971ee7b7f5c12a45d0eb70
Jason <jdamundsen@gmail.com>
2025-02-20 16:34:42 -0600

    Change index fields for model table

--- db/migrate/20250217193831_create_recipe_categories.rb
+++ db/migrate/20250217193831_create_recipe_categories.rb
@@ -3,7 +3,7 @@
 class CreateRecipeCategories < ActiveRecord::Migration[8.0]
   def change
     create_table :recipe_categories, id: :uuid do |t|
-      t.belongs_to :recipe, null: false, foreign_key: true, type: :uuid
+      t.references :recipe, null: false, foreign_key: true, type: :uuid
       t.belongs_to :category, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
