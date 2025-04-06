5be6c6d67e7a8f8565ef0c536d1d65e697ae5c29
Jason <jdamundsen@gmail.com>
2025-04-06 17:41:32 -0500

    wip

--- app/operations/recipes/create.rb
+++ app/operations/recipes/create.rb
@@ -15,9 +15,20 @@ module Recipes
     def call
       @user.recipes.create(@params) do |obj|
         obj.recipe_ingredients = @ingredients.map do |ingredient|
-          # IngredientParser.from_string(ingredient)
           RecipeIngredient.new(user_input: ingredient)
         end
+        unless obj.valid?
+          obj.recipe_ingredients.each_with_index do |ingredient, index|
+            next if ingredient.errors.empty?
+
+            ingredient.errors.full_messages.each do |message|
+              obj.errors.add(:recipe_ingredients, message: "RecipeIngredient #{index + 1}: #{message}")
+            end
+          end
+          return obj
+        end
+        obj.save
+        obj
       end
     end
   end
