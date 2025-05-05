d2aa6b1cabba36ba0bfda00150618410e4db17c9
Jason <jdamundsen@gmail.com>
2025-05-04 21:30:29 -0500

    Added recipe show operation rspec test

---
+++ spec/operations/recipes/show_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Recipes::Show do
+  describe '#call' do
+    let(:recipe_a) do
+      create(:recipe,
+      name: 'Pizza',
+      id: '0a485d75-360e-4e05-bc47-0afced3d8090')
+    end
+
+    it 'returns a specific record by id' do
+      result = described_class.call(params: { id: recipe_a.id })
+      expect(result.id).to eq(recipe_a.id)
+    end
+  end
+end
