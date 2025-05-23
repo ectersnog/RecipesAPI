7012ec4c2d9ce252b537d88be432f7a99859ba1f
Jason <jdamundsen@gmail.com>
2025-05-23 17:51:55 -0500

    updated api-docs

--- spec/schemas/users_schema.rb
+++ spec/schemas/users_schema.rb
@@ -50,11 +50,9 @@ module Schemas
             data: {
               type: :object,
               properties: {
-                id: { type: :string,
-                      format: :uuid,
-                      example: '123e4567-e89b-12d3-a456-426614174000' },
                 name: { type: :string },
-                email: { type: :string }
+                email: { type: :string },
+                password: { type: :string }
               }
             }
           }
