2f19b870633c5e59c360086090395462b41ebbfe
Jason <jdamundsen@gmail.com>
2025-06-16 11:57:25 -0500

    added yardoc to RecipeQuery

--- app/queries/recipe_query.rb
+++ app/queries/recipe_query.rb
@@ -1,39 +1,72 @@
 # frozen_string_literal: true
 
 class RecipeQuery
+  # Recipe Query object returns results from scope
+  #
+  # @param scope [ActiveRecord::Relation] the base query to build on defaults to Recipe.all
   def initialize(scope = Recipe.all)
     @scope = scope
   end
 
+  # Filters the recipe scope to only include carb free recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only carb free recipes
   def carb_free
     @scope.where(is_carb_free: true)
   end
 
+  # Filters the recipe scope to only include gluten-free recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only gluten-free recipes
   def gluten_free
     @scope.where(is_gluten_free: true)
   end
 
+  # Filters the recipe scope to only include kosher recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only kosher recipes
   def kosher
     @scope.where(is_kosher: true)
   end
 
+  # Filters the recipe scope to only include paleo recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only paleo recipes
   def paleo
     @scope.where(is_paleo: true)
   end
 
+  # Filters the recipe scope to only include vegan recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only vegan recipes
   def vegan
     @scope.where(is_vegan: true)
   end
 
+  # Filters the recipe scope to only include vegetarian recipes.
+  #
+  # @return [ActiveRecord::Relation] a scope containing only vegetarian recipes
   def vegetarian
     @scope.where(is_vegetarian: true)
   end
 
+  # Filters the recipe scope to only include the ingredients in the array recipes.
+  #
+  # @param ingredients [Array<String>] ingredients to return results from
+  # @example RecipeQuery.new.ingredient(["cheese", "beef"])
+  #
+  # @return [ActiveRecord::Relation] a scope containing only the ingredients in the array recipes
   def ingredient(ingredients = [])
     ingredient_ids = Ingredient.where(name: ingredients).pluck(:id)
     @scope.joins(:recipe_ingredients).where(recipe_ingredients: { ingredient_id: ingredient_ids })
   end
 
+  # Return a paginated list of results
+  #
+  # @param page [Integer] The page of results to have returned
+  # @param per_page [Integer] The amount of results per page to have returned
+  #
+  # @return [ActiveRecord::Relation] a scope containing only kosher recipes
   def paginate(page = 1, per_page = 10)
     @scope.order(:name).page(page).per(per_page)
   end
